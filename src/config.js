12d072ec75e5c426dcc6d86229c46ab22bb61c81
enrique.ortuno <enrique@ortuno.net>
2018-05-13 00:24:41 -0400

    Adding AWS-Amplify and setting things up.

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-enrique"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: " https://9jisd2iwyj.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_7RVjoqdPt",
+      APP_CLIENT_ID: "88pa0tnd8vc9v12p1bi9flbc8",
+      IDENTITY_POOL_ID: "20fd4fe1-185f-442d-9a49-b9037c001d56"
+    }
+  };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,33 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Amplify from "aws-amplify";
+import config from "./config";
 import './index.css';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true, //is set to true because we want our users to be signed in before they can interact with our app.
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes", //is basically telling Amplify that we want to name our API.
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
   <Router>
