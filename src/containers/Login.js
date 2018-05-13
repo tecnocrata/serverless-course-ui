9487e55e4d2898844e8c4496c328d77745cbacb2
enrique.ortuno <enrique@ortuno.net>
2018-05-13 00:53:17 -0400

    Login with AWS Cognito

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Auth } from "aws-amplify";
 import "./Login.css";
 
 export default class Login extends Component {
@@ -22,8 +23,15 @@ export default class Login extends Component {
     });
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+  
+    try {
+      await Auth.signIn(this.state.email, this.state.password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   //Well explanation about this file in: https://serverless-stack.com/chapters/create-a-login-page.html
