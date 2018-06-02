5ecb9fdad5e47389ba66633931a8b830caadd4c2
enrique.ortuno <enrique@ortuno.net>
2018-06-02 19:46:19 -0400

    Create a Route That Redirects in case the user is authenticated or not

--- src/components/AppliedRoute.js
+++ src/components/AppliedRoute.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
 //Goog explanation about this file: https://serverless-stack.com/chapters/add-the-session-to-the-state.html
 export default ({ component: C, props: cProps, ...rest }) =>
